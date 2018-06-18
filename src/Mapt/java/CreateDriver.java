b0caa6e0d49e3d2247ce704e9d6b46f921541e59
Jeff Lai <jlai3214@gmail.com>
2018-06-17 23:45:56 -0700

    1st update

--- src/Mapt/java/CreateDriver.java
+++ src/Mapt/java/CreateDriver.java
@@ -1,5 +1,28 @@
 package Mapt.java;
 
+/**
+ * Selenium Singleton Class
+ *
+ * @author CarlCocchiaro
+ *
+ */
+@SuppressWarnings("varargs")
 public class CreateDriver {
 
+    // constructor
+    private CreateDriver() {
+    }
+
+    /**
+     * getInstance method to retrieve active driver instance
+     *
+     * @return CreateDriver
+     */
+    public static CreateDriver getInstance() {
+        if ( instance == null ) {
+            instance = new CreateDriver();
+        }
+
+        return instance;
+    }
 }
